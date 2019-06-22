2cdf8ff1682155ab6d701102b578b6ccdee59397
gustav lundstrom <gustav.lundstrom@appliedtechnology.se>
2019-06-22 22:36:21 +0200

    Refactor code

--- src/js/todo.js
+++ src/js/todo.js
@@ -1,105 +1,169 @@
 /* Todo app javascript */
 window.onload = () => {
 
+  buildHtml();
+  
   if (!localStorage.todoList) setInitialState();
-  else {
-    const state = getCurrentState();
-    const list = document.getElementById('todo-list');
-    state.list.forEach(({ title, description, id, completed }) => {
-      const card = createCard(title, description, id, completed);
-      list.append(card);
+  else populateDom();
+
+  function populateDom() {
+    const todoList = getTodoList();
+    todoList.items.forEach(({ title, description, id, state }) => {
+      const todoElement = createTodoElement(title, description, id, state);
+      addToDom(todoElement, state);
     });
   }
 
   const form = document.getElementById('create-todo');
   
   form.onsubmit = event => {
-    const list = document.getElementById('todo-list');
     const title = document.getElementById('title').value;
     const description = document.getElementById('description').value;
+
     if (!title) return;
-    else event.preventDefault();
-    const id = addCardState(title, description);
-    const listItem = createCard(title, description, id);
-    list.append(listItem);
-    emptyForm();
+    
+    event.preventDefault();
+    const { id } = createTodo(title, description);
+    const todoElement = createTodoElement(title, description, id);
+    addToDom(todoElement);
+    resetForm();
   }
 
-  function addCardState(cardTitle, cardDescription) {
-    let state = getCurrentState();
-    const cardState = {
-      id: state.list.length ? state.list[state.list.length - 1].id + 1 : state.list.length,
-      title: cardTitle,
-      description: cardDescription,
-      completed: false
+  function addToDom(item, state) {
+    const todoListElement = document.getElementById('todo-list');
+    const completedListElement = document.getElementById('completed-list');  
+    if (state === 'done') completedListElement.insertAdjacentElement('afterbegin', item);
+    else todoListElement.insertAdjacentElement('afterbegin', item);
+  }
+
+  function createTodo(todoTitle, todoDescription) {
+    const todoList = getTodoList();
+    const { items } = todoList;
+    
+    const todo = {
+      id: items.length ? items[items.length - 1].id + 1 : 0,
+      title: todoTitle,
+      description: todoDescription,
+      state: 'active'
     }
-    state.list.push(cardState);
-    localStorage.setItem('todoList', JSON.stringify(state));
-    return cardState.id;
+
+    items.push(todo);
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+
+    return todo;
   }
 
-  function createCard(title, description, id, completed) {
-    const listItem = document.createElement('LI');
-    if (completed) listItem.classList.add('--marked');
-    listItem.addEventListener('click', handleClick);
-    listItem.setAttribute('id', id);
-    let card = `<div>`;
-    card += `<h3>${title}</h3>`;
-    card += `<p>${description}</p>`;
-    card += `<button class="remove-todo" type="button">Remove item</button>`;
-    card += `</div>`;
-    listItem.innerHTML = card;
-    return listItem;
+  function createTodoElement(title, description, id, state) {
+    const todoElement = document.createElement('LI');
+    if (state === 'done') todoElement.classList.add('--marked');
+    todoElement.addEventListener('click', handleTodoClick);
+    todoElement.id = id;
+
+    let todoHtml = '';
+    todoHtml += `<div>`;
+    todoHtml += `<h3>${title}</h3>`;
+    todoHtml += description ? `<p>${description}</p>` : '';
+    todoHtml += `<button class="remove-todo" type="button">Remove item</button>`;
+    todoHtml += `</div>`;
+    todoElement.innerHTML = todoHtml;
+
+    return todoElement;
   }
 
-  function handleClick(event) {
-    if (event.target.type === 'button') removeCard(event);
-    else markAsDone(event);
-  }
-
-  function markAsDone(event) {
-    const listItem = event.currentTarget;
-    const list = listItem.parentNode;
-    listItem.classList.toggle('--marked');
-    const state = getCurrentState();
-    state.list = state.list.map(elem => {
-      if (elem.id === parseInt(listItem.id)) elem.completed = !elem.completed;
-      return elem;
-    });
-    localStorage.setItem('todoList', JSON.stringify(state));
-
-    if (listItem.classList.contains('--marked')) {
-      list.removeChild(listItem);
-      list.append(listItem);
+  function handleTodoClick({ target, currentTarget }) {
+    if (target.classList.contains('remove-todo')) {
+      const todo = firstAncestorOfType(target, 'li');
+      removeTodo(todo);
     } else {
-      list.insertAdjacentElement('afterbegin', listItem);
+      const todo = currentTarget;
+      todo.classList.toggle('--marked');
+      if (todo.classList.contains('--marked')) markAsDone(todo);
+      else makeActive(todo);
     }
   }
 
-  function removeCard(event) {
-    let state = getCurrentState();
-    const elem = event.target;
-    const listItem = elem.parentElement.parentElement;
-    const newStateList = state.list.filter(elem => elem.id !== parseInt(listItem.id));
-    state.list = newStateList;
-    localStorage.setItem('todoList', JSON.stringify(state));
-    listItem.parentNode.removeChild(listItem);
+  function firstAncestorOfType(elem, ancestorElemType) {
+    let ancestorFound = false;
+    while (!ancestorFound) {
+      if (!elem.parentElement) {
+        console.error(`${elem.nodeName} element doesn\'t have an ancestor of type ${ancestorElemType}.`);
+        break;
+      }
+      else if (elem.parentElement.nodeName.toLowerCase() === ancestorElemType.toLowerCase()) {
+        ancestorFound = !ancestorFound;
+        return elem.parentElement;
+      } 
+      elem = elem.parentElement;
+    }
   }
 
-  window.removeCard = removeCard;
+  function makeActive(todo) {
+    updateTodoState(todo.id, 'active');
+    todo.parentNode.removeChild(todo);
+    addToDom(todo, 'active');
+  }
+  
+  function markAsDone(todo) {
+    updateTodoState(todo.id, 'done');
+    todo.parentNode.removeChild(todo);
+    addToDom(todo, 'done');
+  }
 
-  function getCurrentState() {
+  function updateTodoState(id, newState = 'active') {
+    const todoList = getTodoList();
+    let { items } = todoList;
+    
+    items = items.map(item => {
+      if (item.id === parseInt(id)) item.state = newState;
+      return item;
+    });
+
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+  }
+
+  function removeTodo(todo) {
+    let { items } = getTodoList();
+    items = items.filter(elem => elem.id !== parseInt(todo.id));
+    const newTodoList = { items: [...items] };
+    localStorage.setItem('todoList', JSON.stringify(newTodoList));
+    todo.parentElement.removeChild(todo);
+  }
+
+  function getTodoList() {
     let stateObj = JSON.parse(localStorage.getItem('todoList'));
     return stateObj;
   }
   
   function setInitialState() {
-    let stateObj = `{"list":[]}`;
+    let stateObj = `{"items":[]}`;
     localStorage.setItem('todoList', stateObj);
   }
   
-  function emptyForm() {
+  function resetForm() {
     const inputElements = document.querySelectorAll('INPUT');
     inputElements.forEach(elem => elem.value = '');
+  }
+
+  function buildHtml() {
+    const presentationSection = document.getElementById('todo-presentation');
+    const creationSection = document.getElementById('todo-creation');
+
+    presentationSection.innerHTML =
+      `<h2>My todo items</h2>
+      <ul id="todo-list"></ul>
+      <h2>My completed items</h2>
+      <ul id="completed-list"></ul>`;
+
+    creationSection.innerHTML =
+      `<form id="create-todo" autocomplete="off">
+        <fieldset>
+          <legend>Create new list item</legend>
+          <label for="title">Title</label>
+          <input id="title" type="text" name="title" placeholder="Buy eggs..." required />
+          <label for="description">Description</label>
+          <input id="description" type="text" name="description" placeholder="Needed for pancakes..." />
+          <button type="submit" form="create-todo">Add item</button>
+        </fieldset>
+      </form>`;
   }
 }
