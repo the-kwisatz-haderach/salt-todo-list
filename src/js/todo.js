d01f8c3fcdc378ae79ec405871f0c1ceef888c15
gustav lundstrom <gustav.lundstrom@appliedtechnology.se>
2019-06-24 12:48:17 +0200

    Start CSS and refactor js

--- src/js/todo.js
+++ src/js/todo.js
@@ -1,89 +1,146 @@
 /* Todo app javascript */
 window.onload = () => {
-
-  buildHtml();
-  
-  if (!localStorage.todoList) createInitialState();
-  else renderNewList();
-
-  function renderNewList() {
-    clearListsHtml();
-    const { items, sortOrder } = getTodoList();
-    
-    items
-      .sortByProperty('timestamp', sortOrder)
-      .forEach(({ title, description, id, state, timestamp }) => {
-        const todoElement = createTodoElement(title, description, id, state, timestamp);
-        addToDom(todoElement, state);
-    });
+  function createInitialState() {
+    const stateObj = { items: [], sortOrder: 'descending' };
+    localStorage.setItem('todoList', JSON.stringify(stateObj));
   }
 
-  const sortSelector = document.getElementById('sort-order');
-  sortSelector.onchange = event => {
-    changeSortOrder(event);
-    renderNewList();
+  function getTodoList() {
+    const stateObj = JSON.parse(localStorage.getItem('todoList'));
+    return stateObj;
   }
 
-  const form = document.getElementById('create-todo');
-  form.onsubmit = event => {
-    const title = document.getElementById('title').value;
-    const description = document.getElementById('description').value;
-    if (!title) return;
-    
-    event.preventDefault();
-
-    createTodo(title, description);
-    resetForm();
-    renderNewList();
+  function getTodoState(id) {
+    const { items } = getTodoList();
+    const todo = items.find(item => item.id === parseInt(id, 10));
+    return todo.state;
   }
 
-  function addToDom(item, state) {
-    const todoListElement = document.getElementById('todo-list');
-    const completedListElement = document.getElementById('completed-list');
-    if (state === 'done') completedListElement.insertAdjacentElement('afterbegin', item);
-    else todoListElement.insertAdjacentElement('afterbegin', item);
+  function removeTodo(todo) {
+    const todoList = getTodoList();
+    todoList.items = todoList.items.filter(elem => elem.id !== parseInt(todo.id, 10));
+    localStorage.setItem('todoList', JSON.stringify(todoList));
   }
 
   function createTodo(todoTitle, todoDescription) {
     const todoList = getTodoList();
     const { items } = todoList;
-    
+
     const todo = {
       id: items.length ? items[items.length - 1].id + 1 : 0,
       title: todoTitle,
       description: todoDescription,
       state: 'active',
-      timestamp: Date.now()
-    }
+      timestamp: Date.now(),
+    };
 
     items.push(todo);
     localStorage.setItem('todoList', JSON.stringify(todoList));
   }
 
+  function updateTodoState(id, newState = 'active') {
+    const todoList = getTodoList();
+    todoList.items = todoList.items.map((item) => {
+      if (item.id === parseInt(id, 10)) item.state = newState;
+      return item;
+    });
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+  }
+
+  function changeSortOrder({ currentTarget }) {
+    const todoList = getTodoList();
+    todoList.sortOrder = currentTarget.value;
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+  }
+
+  function buildHtml() {
+    const presentationSection = document.getElementById('todo-presentation');
+    const creationSection = document.getElementById('todo-creation');
+    const sortOrder = getTodoList() ? getTodoList().sortOrder : undefined;
+
+    presentationSection.innerHTML = `<label for="sort-order">Sort order</label> 
+      <select id="sort-order">
+        ${sortOrder === 'ascending'
+    ? `<option value="ascending">Ascending</option>
+          <option value="descending">Descending</option>`
+    : `<option value="descending">Descending</option>
+          <option value="ascending">Ascending</option>`}
+      </select>
+      <button type="button" onclick="clearAll();">Clear all</button>
+      <ul id="todo-list"></ul>
+      <ul id="completed-list"></ul>`;
+
+    creationSection.innerHTML = `<form id="create-todo" autocomplete="off">
+        <fieldset>
+          <legend>Create new list item</legend>
+          <label for="title">Title</label>
+          <input id="title" type="text" name="title" placeholder="Buy eggs..." required />
+          <label for="description">Description</label>
+          <input id="description" type="text" name="description" placeholder="Needed for pancakes..." />
+          <button type="submit" form="create-todo">Add item</button>
+        </fieldset>
+      </form>`;
+  }
+
+  function clearListsHtml() {
+    const todoList = document.getElementById('todo-list');
+    const completedList = document.getElementById('completed-list');
+    todoList.innerHTML = '';
+    completedList.innerHTML = '';
+  }
+
+  function clearAll() {
+    const todoList = getTodoList();
+    todoList.items = [];
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+    clearListsHtml();
+  }
+  window.clearAll = clearAll;
+
+  function resetForm() {
+    const inputElements = document.querySelectorAll('INPUT');
+    inputElements.forEach(elem => elem.value = '');
+  }
+
+  function addToDom(item, state) {
+    const todoListElement = document.getElementById('todo-list');
+    const completedListElement = document.getElementById('completed-list');
+    item.addEventListener('click', handleTodoClick);
+    if (state === 'done') completedListElement.insertAdjacentElement('afterbegin', item);
+    else todoListElement.insertAdjacentElement('afterbegin', item);
+  }
+
   function createTodoElement(title, description, id, state, timestamp) {
-    timestamp = new Date().toDateString();
+    const createdAt = new Date(timestamp).toDateString();
 
     const todoElement = document.createElement('LI');
     todoElement.classList.add(`--${state}`);
-    todoElement.addEventListener('click', handleTodoClick);
     todoElement.id = id;
 
     let todoHtml = '';
-    todoHtml += `<div>`;
+    todoHtml += '<div>';
     todoHtml += `<h3>${title}</h3>`;
     todoHtml += description ? `<p>${description}</p>` : '';
-    todoHtml += `<button class="remove-todo" type="button">Remove item</button>`;
-    todoHtml += `<time datetime="${timestamp}">Created ${timestamp}</time>`;
-    todoHtml += `</div>`;
+    todoHtml += state === 'done' ? '<button class="remove-todo" type="button">Remove item</button>' : '';
+    todoHtml += `<time datetime="${createdAt}">Created ${createdAt}</time>`;
+    todoHtml += '</div>';
     todoElement.innerHTML = todoHtml;
 
     return todoElement;
   }
 
-  function getTodoState(id) {
-    const { items } = getTodoList();
-    const todo = items.find(item => item.id === parseInt(id));
-    return todo.state;
+  function renderNewList() {
+    clearListsHtml();
+    const { items, sortOrder } = getTodoList();
+
+    items
+      .sortByProperty('timestamp', sortOrder)
+      .forEach(({
+        title, description, id, state, timestamp,
+      }) => {
+        const todoElement = createTodoElement(title, description, id, state, timestamp);
+        addToDom(todoElement, state);
+      });
   }
 
   function handleTodoClick({ target, currentTarget }) {
@@ -98,121 +155,38 @@ window.onload = () => {
     renderNewList();
   }
 
-  function changeSortOrder({ currentTarget }) {
-    todoList = getTodoList();
-    todoList.sortOrder = currentTarget.value;
-    localStorage.setItem('todoList', JSON.stringify(todoList));
-  }
 
-  function updateTodoState(id, newState = 'active') {
-    const todoList = getTodoList();
-    todoList.items = todoList.items.map(item => {
-      if (item.id === parseInt(id)) item.state = newState;
-      return item;
-    });
-    localStorage.setItem('todoList', JSON.stringify(todoList));
-  }
-  
-  function removeTodo(todo) {
-    const todoList = getTodoList();
-    todoList.items = todoList.items.filter(elem => elem.id !== parseInt(todo.id));
-    localStorage.setItem('todoList', JSON.stringify(todoList));
-  }
+  buildHtml();
 
-  function clearAll() {
-    const todoList = getTodoList();
-    todoList.items = [];
-    localStorage.setItem('todoList', JSON.stringify(todoList));
-    clearListsHtml();
-  }
-  
-  window.clearAll = clearAll;
+  if (!localStorage.todoList) createInitialState();
+  else renderNewList();
 
-  function clearListsHtml() {
-    const todoList = document.getElementById('todo-list');
-    const completedList = document.getElementById('completed-list');
-    todoList.innerHTML = '';
-    completedList.innerHTML = '';
-  }
+  const sortSelector = document.getElementById('sort-order');
+  sortSelector.onchange = (event) => {
+    changeSortOrder(event);
+    renderNewList();
+  };
 
-  function getTodoList() {
-    let stateObj = JSON.parse(localStorage.getItem('todoList'));
-    return stateObj;
-  }
-  
-  function createInitialState() {
-    let stateObj = JSON.stringify({ items: [], sortOrder: 'descending' });
-    localStorage.setItem('todoList', stateObj);
-  }
-  
-  function resetForm() {
-    const inputElements = document.querySelectorAll('INPUT');
-    inputElements.forEach(elem => elem.value = '');
-  }
+  const form = document.getElementById('create-todo');
+  form.onsubmit = (event) => {
+    const title = document.getElementById('title').value;
+    const description = document.getElementById('description').value;
+    if (!title) return;
 
-  function buildHtml() {
-    const presentationSection = document.getElementById('todo-presentation');
-    const creationSection = document.getElementById('todo-creation');
-    const sortOrder = getTodoList() ? getTodoList().sortOrder : undefined;
+    event.preventDefault();
 
-    presentationSection.innerHTML =
-      `<h2>My todo items</h2>
-      <label for="sort-order">Sort order</label> 
-      <select id="sort-order">
-        ${ sortOrder === 'ascending' 
-        ? `<option value="ascending">Ascending</option>
-          <option value="descending">Descending</option>` 
-        : `<option value="descending">Descending</option>
-          <option value="ascending">Ascending</option>` }
-      </select>
-      <button type="button" onclick="clearAll();">Clear all</button>
-      <ul id="todo-list"></ul>
-      <h2>My completed items</h2>
-      <ul id="completed-list"></ul>`;
-
-    creationSection.innerHTML =
-      `<form id="create-todo" autocomplete="off">
-        <fieldset>
-          <legend>Create new list item</legend>
-          <label for="title">Title</label>
-          <input id="title" type="text" name="title" placeholder="Buy eggs..." required />
-          <label for="description">Description</label>
-          <input id="description" type="text" name="description" placeholder="Needed for pancakes..." />
-          <button type="submit" form="create-todo">Add item</button>
-        </fieldset>
-      </form>`;
-  }
-}
-
-// function firstAncestorOfType(elem, ancestorType) {
-//   let ancestorFound = false;
-//   while (!ancestorFound) {
-//     if (!elem.parentElement) {
-//       console.error(`${elem.nodeName} element doesn\'t have an ancestor of type ${ancestorType}.`);
-//       break;
-//     }
-//     else if (elem.parentElement.nodeName.toLowerCase() === ancestorType.toLowerCase()) {
-//       ancestorFound = !ancestorFound;
-//       return elem.parentElement;
-//     } 
-//     elem = elem.parentElement;
-//   }
-// }
-
-console.log('remove me!');
+    createTodo(title, description);
+    resetForm();
+    renderNewList();
+  };
+};
 
 function sortByProperty(property, order) {
   switch (order) {
     case 'descending':
-      return this.sort((curr, next) => 
-          curr[property] > next[property] ? 1 
-        : curr[property] < next[property] ? -1 
-        : 0);
-    case 'ascending':
-      return this.sort((curr, next) => 
-          curr[property] > next[property] ? -1 
-        : curr[property] < next[property] ? 1 
-        : 0);
+      return this.sort((curr, next) => (curr[property] > next[property] ? 1 : curr[property] < next[property] ? -1 : 0));
+    default:
+      return this.sort((curr, next) => (curr[property] > next[property] ? -1 : curr[property] < next[property] ? 1 : 0));
   }
 }
 
